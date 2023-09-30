LineageOS/android_packages_apps_Settings

Settings: Make FRP go through ChooseLockPatternSize if needed
Change-Id: I70b0ddb20033dd5505c391763a0a58fd2f9c8004

src/com/android/settings/password/ChooseLockPatternSize.java
@@ -95,8 +95,10 @@ public class ChooseLockPatternSize extends SettingsActivity {
                 patternSize = 3;
             }
 
-            Intent intent = new Intent(getActivity(), ChooseLockPattern.class);
-            intent.putExtras(getActivity().getIntent().getExtras());
+            Bundle extras = getActivity().getIntent().getExtras();
+            Intent intent = new Intent();
+            intent.setClassName(getActivity(), extras.getString("destination"));
+            intent.putExtras(extras);
             intent.putExtra("pattern_size", patternSize);
             intent.addFlags(Intent.FLAG_ACTIVITY_FORWARD_RESULT);
             startActivity(intent);

xDroidOSS-Pixel/android_platform_packages_apps_settings

src/com/android/settings/password/ChooseLockPatternSize.java
@@ -96,8 +96,10 @@ public class ChooseLockPatternSize extends SettingsActivity {
                 patternSize = 3;
             }
 
-            Intent intent = new Intent(getActivity(), ChooseLockPattern.class);
-            intent.putExtras(getActivity().getIntent().getExtras());
+            Bundle extras = getActivity().getIntent().getExtras();
+            Intent intent = new Intent();
+            intent.setClassName(getActivity(), extras.getString("className"));
+            intent.putExtras(extras);
             intent.putExtra("pattern_size", patternSize);
             intent.addFlags(Intent.FLAG_ACTIVITY_FORWARD_RESULT);
             startActivity(intent);